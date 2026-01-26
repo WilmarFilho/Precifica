e64e5db057b4146b2d411fc09996be5c0b5b5e7f
Wilmar José Alves Ferreira Filho <wilmarfilho32@hotmail.com>
2026-01-26 14:00:47 -0300

    feature: barra de loader ao trocar de rota

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,36 @@
-import NextTopLoader from 'nextjs-toploader';
+import type { Metadata } from "next";
+
+
+import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://precifica.nkwflow.com"),
+  title: "Precifica - Simplificando seus orçamentos",
+  description: "Facilite a criação e gestão de orçamentos com nossa ferramenta intuitiva e eficiente.",
+};
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="pt-BR">
-      <body>
+    <html lang="pt-BR" className="dark">
+      <body className="font-sans antialiased bg-background text-foreground">
 
-        {/* Configuração do Loader de Rota instantâneo */}
-        <NextTopLoader 
-         color="#2563eb"  // Cor azul-600 (a mesma dos seus botões)
+        <NextTopLoader
+          color="#2563eb" // Cor azul-600 (a mesma dos seus botões)
           initialPosition={0.08}
           crawlSpeed={200}
           height={3}
           crawl={true}
-          showSpinner={false}
+          showSpinner={false} // Desative o spinner se quiser apenas a barra
           easing="ease"
           speed={200}
           shadow="0 0 10px #2563eb,0 0 5px #2563eb"
-          zIndex={99999}
         />
-        
+
         {children}
       </body>
     </html>
